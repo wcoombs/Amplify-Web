a4352415f81f1e17d241503219cc39deb35f6002
Robert Laurin <robert.laurin89@gmail.com>
2018-01-16 09:52:42 -0600

    Modify which assets are created by generators

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,11 @@ module Amplify
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+      g.channel         assets: false
+    end
   end
 end
