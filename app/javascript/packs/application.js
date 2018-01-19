1c1091b766ff02eb23bb1e5060b18d1c9986d6ec
Robert Laurin <robert.laurin89@gmail.com>
2018-01-18 19:40:23 -0600

    Add semantic ui to project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,5 +6,11 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
+import $ from 'jquery';
 import "./application.css";
 
+window.$ = $;
+window.jQuery = $;
+
+require('semantic-ui-css/semantic.js');
+require('semantic-ui-css/semantic.css');
