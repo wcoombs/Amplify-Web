d343023254c8d3970fb9438c3a356ddf6220b8e3
Robert Laurin <robert.laurin89@gmail.com>
2018-01-18 19:49:24 -0600

    Improve custom css compilation folder structure

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 import $ from 'jquery';
-import "./application.css";
+import "application.css";
 
 window.$ = $;
 window.jQuery = $;
