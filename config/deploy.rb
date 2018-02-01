e9bffde4d675efa9a9ea32eb10f5f4d4427f5172
Robert Laurin <robert.laurin89@gmail.com>
2018-02-01 14:01:12 -0600

    Include secrets.yml, don't hard code the stage

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :puma_workers,    0
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
-set :stage,           :production
+
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
@@ -31,7 +31,7 @@ set :branch, ENV['CAP_BRANCH'] || :master # eg: CAP_BRANCH=custom-branch cap san
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-set :linked_files, %w{config/database.yml}
+set :linked_files, fetch(:linked_files, []).push('config/secrets.yml', 'config/database.yml')
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
