c8d2e84e54b18b4af0074304050455d9de9918a9
Robert Laurin <robert.laurin89@gmail.com>
2018-02-04 13:27:40 -0600

    Update lead signup to allow anything and always thank them

--- spec/controllers/lead_signup_controller_spec.rb
+++ spec/controllers/lead_signup_controller_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe LeadSignupController, type: :controller do
       expect(response).to have_http_status(:ok)
     end
   end
+
+  describe "POST#create" do
+    it "creates a new lead" do
+      expect do
+        process(:create, params: { lead: { email: "robert@example.com", referrer: nil } })
+      end.to change { Lead.count }.by(1)
+
+      expect(flash[:notice]).to eq("Thanks for signing up, you'll be the first to know when we launch!")
+      expect(response).to redirect_to(new_lead_signup_path)
+    end
+  end
 end
