fcf0916c236997cb5e9fc43d192334889d835d2b
Rebecca <becca.tiessen@gmail.com>
2018-02-07 17:48:33 -0600

    Add mock data to song list view and render it nicely

--- app/controllers/song_list_controller.rb
+++ app/controllers/song_list_controller.rb
@@ -1,4 +1,7 @@
 class SongListController < ApplicationController
   def index
+    # this will eventually get song data from database, then we can pass votes into the view as well
+    @songs = ["Africa", "Come Sail Away", "The Final Countdown", "Lake Shore Drive",
+    "Mr. Blue Sky", "Twist and Shout", "Viva La Vida", "Toxic"]
   end
 end
