82879c2f7256498ccddfaeae6501ff95498c2c55
Julia <stoykojulia@gmail.com>
2018-02-07 21:05:24 -0600

    Add song list

--- app/controllers/song_list_controller.rb
+++ app/controllers/song_list_controller.rb
@@ -2,6 +2,10 @@ class SongListController < ApplicationController
   def index
     # this will eventually get song data from database, then we can pass votes into the view as well
     @songs = ["Africa", "Come Sail Away", "The Final Countdown", "Lake Shore Drive",
-    "Mr. Blue Sky", "Twist and Shout", "Viva La Vida", "Toxic"]
+    "Mr. Blue Sky", "Twist and Shout", "Viva La Vida", "Toxic","Africa", "Come Sail Away", "The Final Countdown", "Lake Shore Drive",
+              "Mr. Blue Sky", "Twist and Shout", "Viva La Vida", "Toxic","Africa", "Come Sail Away", "The Final Countdown", "Lake Shore Drive",
+              "Mr. Blue Sky", "Twist and Shout", "Viva La Vida", "Toxic","Africa", "Come Sail Away", "The Final Countdown", "Lake Shore Drive",
+              "Mr. Blue Sky", "Twist and Shout", "Viva La Vida", "Toxic","Africa", "Come Sail Away", "The Final Countdown", "Lake Shore Drive",
+              "Mr. Blue Sky", "Twist and Shout", "Viva La Vida", "Toxic", "All Star"]
   end
 end
