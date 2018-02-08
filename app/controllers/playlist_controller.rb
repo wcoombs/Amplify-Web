1839097f9ba9cd1bd5426a156dcd5ea87d7277b5
Robert Laurin <robert.laurin89@gmail.com>
2018-02-08 12:18:37 -0600

    Fix failing playlist render test

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -1,6 +1,6 @@
 class PlaylistController < ApplicationController
-    def show
-        room = Room.find(params[:id])
-        @songs = room.songs
-    end
+  def show
+    room = Room.find(params[:id])
+    @songs = room.songs
+  end
 end
--- spec/controllers/playlist_controller_spec.rb
+++ spec/controllers/playlist_controller_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe PlaylistController, type: :controller do
+  let(:room_a) { rooms(:room_a) }
+
   describe "GET#show" do
     it "renders the page" do
-      process(:new)
+      process(:show, params: { id: room_a.id })
       expect(response).to have_http_status(:ok)
     end
   end
