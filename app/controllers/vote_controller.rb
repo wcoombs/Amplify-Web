285975e604950c9c1aba0a12455a1c2e2b6f0633
Robert Laurin <robert.laurin89@gmail.com>
2018-02-08 12:07:49 -0600

    Add tests using fixtures for voter signup controller

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -2,7 +2,6 @@ class VoteController < ApplicationController
   def update
     voter = Voter.find(session[:voter_id])
     song = Song.find(params[:id])
-
     vote = Vote.find_or_initialize_by(voter: voter, song: song)
     vote.update(score: params[:vote])
   end
--- spec/controllers/playlist_controller_spec.rb
+++ spec/controllers/playlist_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe PlaylistController, type: :controller do
-
+  describe "GET#show" do
+    it "renders the page" do
+      process(:new)
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
