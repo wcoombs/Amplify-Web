e835b4bcca03ab16f0cc587b520a497b92ac4ea6
Will Coombs <william.m.coombs@gmail.com>
2018-02-08 20:09:29 -0600

    Bypassing auth so that mobile can talk to the local server

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,6 @@
 class RoomsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
     respond_to do |format|
       format.json { render json: { room_code: "ola" }, status: :ok }
