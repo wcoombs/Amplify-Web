e4e1002649aba4449eca7a842756a2bc1f52cb9c
Elizabeth <lizkenyon07@gmail.com>
2018-02-10 22:35:14 -0600

    add empty and nil room validation

--- app/controllers/voter_signups_controller.rb
+++ app/controllers/voter_signups_controller.rb
@@ -4,10 +4,13 @@ class VoterSignupsController < ApplicationController
 
   def create
     room = Room.find_by(room_code: params[:room_code])
-    return redirect_to new_voter_signup_path if room.nil?
-
-    voter = Voter.create(room: room, nickname: params[:nickname])
-    session[:voter_id] = voter.id
-    redirect_to playlist_path(room)
+    if room.nil?
+      flash[:room_notice] = "Ooops, no room matches that code."
+      render :action => :new
+    else
+      voter = Voter.create(room: room, nickname: params[:nickname])
+      session[:voter_id] = voter.id
+      redirect_to playlist_path(room)
+    end
   end
 end
