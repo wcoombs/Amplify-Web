48769584402acedd5747c47b253774b6338a3f5e
Julia <stoykojulia@gmail.com>
2018-02-11 21:59:50 -0600

    Hit vote endpoint

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -4,13 +4,10 @@ class PlaylistController < ApplicationController
   def show
     room = Room.find(params[:id])
     @songs = room.songs
+    @room_id = room.id
   end
 
-  def up_vote
-    logger.info "Up vote!"
-    Vote.create({voter_id: 1, song_id: 1, score: 1})
-  end
-  def down_vote
-    Vote.create({voter_id: 1, song_id: 2, score: -1})
+  def vote
+
   end
 end
