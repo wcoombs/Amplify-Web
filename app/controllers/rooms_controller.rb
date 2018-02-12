7e2313056d75b67f3c3d93015013747f6557408b
lizkenyon <lizkenyon07@gmail.com>
2018-02-11 22:29:46 -0600

    Data Generator (#20)
    * simple data generator
    
    * remove code equality check in test
    
    * update tests

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,14 @@
 class RoomsController < ApplicationController
+  include Data_Generator
   skip_before_action :verify_authenticity_token
 
+
   def create
+    new_room = Room.create(room_code: SecureRandom.hex(2).upcase)
+    add_songs(new_room.id)
+
     respond_to do |format|
-      format.json { render json: { room_code: "ola" }, status: :ok }
+      format.json { render json: { room_code: new_room.room_code }, status: :ok }
       format.html { head :forbidden }
     end
   end
