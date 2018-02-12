1648f94c8c587f97a4cedb7637bc537980de6eb7
Julia <stoykojulia@gmail.com>
2018-02-11 21:13:20 -0600

    Linking up_vote

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -1,7 +1,16 @@
 class PlaylistController < ApplicationController
+  helper_method :up_vote, :down_vote
+
   def show
     room = Room.find(params[:id])
     @songs = room.songs
   end
 
+  def up_vote
+    logger.info "Up vote!"
+    Vote.create({voter_id: 1, song_id: 1, score: 1})
+  end
+  def down_vote
+    Vote.create({voter_id: 1, song_id: 2, score: -1})
+  end
 end
