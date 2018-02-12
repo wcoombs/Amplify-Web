3c869c78de337db977c789d2dc0e8a50b588b92d
Elizabeth <lizkenyon07@gmail.com>
2018-02-11 21:22:24 -0600

    simple data generator

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,16 @@
 class RoomsController < ApplicationController
+  include Data_Generator
   skip_before_action :verify_authenticity_token
 
+
   def create
+    new_room = Room.new
+    new_room.room_code = SecureRandom.hex(2).upcase
+    new_room.save
+    add_songs(new_room.id)
+
     respond_to do |format|
-      format.json { render json: { room_code: "ola" }, status: :ok }
+      format.json { render json: { room_code: new_room.room_code }, status: :ok }
       format.html { head :forbidden }
     end
   end
