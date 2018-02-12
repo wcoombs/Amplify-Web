f474f70f557ba0df1446d1d1ef8672d20047f801
Julia <stoykojulia@gmail.com>
2018-02-11 21:59:50 -0600

    Hit vote endpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-Voter.create([{room_id: 1}, {room_id: 1}])
-Vote.create([{score: 1, song_id: 1, voter_id: 1}, {score: -1, song_id: 2, voter_id: 2}, {score: 1, song_id: 1, voter_id: 1}, {score: -1, song_id: 2, voter_id: 2}])
 songs = Song.create([{ title: 'Toxic' }, {title: 'Wonderwall'}, {title: 'Take Me to Church'}])
 Room.create([{songs: songs}])
+Voter.create([{room_id: 13}, {room_id: 13}])
+Vote.create([{score: 1, song_id: 34, voter_id: 9}, {score: -1, song_id: 34, voter_id: 10}, {score: 1, song_id: 35, voter_id: 9}, {score: -1, song_id: 35, voter_id: 10}])
