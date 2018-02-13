013b23de541423442aae4f8a71a118045faccc55
Andy Lun <andylun1206@gmail.com>
2018-02-12 22:09:24 -0600

    Creating a destroy room endpoint (#31)
    * Adding destroy room endpoint
    
    * Updating README
    
    * Adding a destroy test

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe RoomsController, type: :controller do
+  let(:room_a) { rooms(:room_a) }
+
   describe "POST#create" do
     it "responds to a json request" do
       process(:create, format: :json)
@@ -9,6 +11,17 @@ RSpec.describe RoomsController, type: :controller do
       expect(response).to have_http_status(:ok)
       expect(json["room_code"]).to be_present
       expect(Room.last.songs.count).to eq(5)
+    end
+  end
+
+  describe "DELETE#destroy" do
+    it "responds to a delete request" do
+      num_rooms = Room.count
+
+      process(:destroy, format: :json, params: { id: room_a.id })
+
+      expect(response).to have_http_status(:ok)
+      expect(Room.count).to eq(num_rooms - 1)
     end
   end
 end
