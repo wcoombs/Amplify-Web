1b29503c2d281a633a1ab1ffb4bbb950c7173368
Julia <stoykojulia@gmail.com>
2018-02-12 21:39:37 -0600

    Change colour based on user's vote

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -1,7 +1,19 @@
 class PlaylistController < ApplicationController
+  helper_method :has_voted
+
   def show
     room = Room.find(params[:id])
     @songs = room.songs
     @room_id = room.id
+  end
+
+  def has_voted(song_id)
+    @song = Song.find_by_id(song_id)
+    @song.votes.each do |vote|
+      if vote.voter_id === session[:voter_id]
+        return vote.score
+      end
+    end
+    return 0
   end
 end
