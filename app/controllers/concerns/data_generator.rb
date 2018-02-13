649de501cd260a63affbe98e9821fd47a18f73d9
Rebecca <becca.tiessen@gmail.com>
2018-02-12 20:57:20 -0600

    Add artist to data generator and create database migration to add artist column

--- app/controllers/concerns/data_generator.rb
+++ app/controllers/concerns/data_generator.rb
@@ -2,36 +2,38 @@ module Data_Generator
   extend ActiveSupport::Concern
 
   SONG_LIST = [
-      "Toxic",
-      "Bad Blood",
-      "Never Gonna Give You Up",
-      "Champagne Supernova",
-      "Wonderwall",
-      "Bad Touch",
-      "Pump It",
-      "Violin Concerto in F Minor",
-      "Jump on It",
-      "Shake it Off",
-      "Crazy in Love",
-      "Prytania",
-      "White Blank Page",
-      "Womanizer",
-      "Umbrella",
-      "Hollaback Girl",
-      "Burnin' Up",
-      "Buttons",
-      "Fishin' in the Dark",
-      "Tik Tok",
-      "Vindicated",
-      "Sugar We're Going Down",
-      "Monkey Wrench",
-      "Wake Me Up When September Ends",
+      {title: "Toxic", artist: "Britney Spears"},
+      {title: "Bad Blood", artist: "T-Swift"},
+      {title: "Buttons", artist: "Pussycat Dolls"},
+      {title: "Wake Me Up When September Ends", artist: "Green Day"},
+      {title: "Never Gonna Give You Up", artist: "Rick Astley"},
+      {title: "Champagne Supernova", artist: "Oasis"},
+      {title: "Wonderwall", artist: "Pink Floyd"},
+      {title: "Bad Touch", artist: "Bloodhound Gang"},
+      {title: "Pump It", artist: "The Black Eyed Peas"},
+      {title: "Violin Concerto in F Minor", artist: "Antonio Vivaldi"},
+      {title: "Jump on It", artist: "Sugarhill Gang"},
+      {title: "Shake it Off", artist: "T-Swift"},
+      {title: "Crazy in Love", artist: "Beyonce"},
+      {title: "Prytania", artist: "??"},
+      {title: "White Blank Page", artist: "Mumford and Sons"},
+      {title: "Womanizer", artist: "Britney Spears"},
+      {title: "Umbrella", artist: "Rihanna"},
+      {title: "Hollaback Girl", artist: "Gwen Stefani"},
+      {title: "Burnin' Up", artist: "Jonas Brothers"},
+      {title: "Fishin' in the Dark", artist: "Nitty Gritty Dirt Band"},
+      {title: "Tik Tok", artist: "Kesha"},
+      {title: "Vindicated", artist: "Dashboard Confessional"},
+      {title: "Sugar We're Going Down", artist: "Fall Out Boy"},
+      {title: "Monkey Wrench", artist: "Foo Fighters"}
   ]
 
   def add_songs(room_id)
     for i in 0..4
       song_num = rand(SONG_LIST.length)
-      Song.create(title: SONG_LIST[song_num], room_id:room_id)
+      Song.create(title: SONG_LIST[song_num][:title],
+                  artist: SONG_LIST[song_num][:artist],
+                  room_id:room_id)
     end
   end
 end
