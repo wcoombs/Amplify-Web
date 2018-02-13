eb143b288094beaee914713972a179ce6d0f186c
Robert Laurin <robert.laurin89@gmail.com>
2018-02-13 12:11:29 -0600

    Add response response to vote controller

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -2,12 +2,12 @@ class VoteController < ApplicationController
   def update
     voter = Voter.find(session[:voter_id])
     unless params[:playlist_id].to_i == voter.room_id
-      return render json: {}, status: :forbidden
+      return render json: { error: "forbidden resource" }, status: :forbidden
     end
 
     song = Song.find(params[:id])
     unless song.room_id == voter.room_id
-      return render json: {}, status: :forbidden
+      return render json: { error: "forbidden resource" }, status: :forbidden
     end
 
     vote = Vote.find_or_initialize_by(voter: voter, song: song)
