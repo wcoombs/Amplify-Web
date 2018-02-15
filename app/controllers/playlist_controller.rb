3bcb933f42ecd9427d34e9868c7205425280e7cc
Julia Stoyko <stoykojulia@gmail.com>
2018-02-15 09:38:26 -0600

    Use map for playlist_data and remove vote when clicked again (#40)

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -8,12 +8,28 @@ class PlaylistController < ApplicationController
       return redirect_to root_path
     end
 
-    voter = @room.voters.select { |v| v.id == session[:voter_id] }
+    voter = @room.voters.detect { |v| v.id == session[:voter_id] }
     unless voter.present?
       flash[:error] = "You must create a user to join this room."
       return redirect_to root_path
     end
+    @playlist_data = playlist_data(@room.songs, voter)
   end
+
+  private
+
+  def playlist_data(songs, voter)
+    songs.each.map do |song|
+      {
+          song_id: song.id,
+          title: song.title,
+          artist: song.artist,
+          voter_score: song.voter_vote(voter)&.score,
+          vote_sum: song.votes.sum(:score)
+      }
+    end
+  end
+
 
   def has_voted(song_id)
     @vote = Vote.where("song_id = ? AND voter_id = ?", song_id, session[:voter_id]).limit(1).pluck(:score)[0]
