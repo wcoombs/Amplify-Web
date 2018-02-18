b81f2499a0356781e270a2935980d3b544ebfa33
Robert Laurin <robert.laurin89@gmail.com>
2018-02-17 18:17:28 -0600

    Clean up response types and comments

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -1,11 +1,11 @@
 class CallbackController < ActionController::Base
   def spotify_callback
     if params[:error].present?
-      render json: { error: params[:error] }
+      render json: { error: params[:error] }, status: :bad_request
     end
 
     unless params[:code].present?
-      render json: { error: "Auth token not provided." }
+      render json: { error: "Auth token required for callback." }, status: :bad_request
     end
 
     tokens = SpotifyApi.new.fetch_tokens(params[:code])
@@ -16,19 +16,6 @@ class CallbackController < ActionController::Base
 
     host = Host.create(email: me["email"])
     spotify_account.update(host: host)
-    render json: { api_token: host.api_token }
+    render json: { api_token: host.api_token }, status: :ok
   end
 end
-
-=begin
-GET https://accounts.spotify.com/authorize/
-?client_id=eca853c70bc24b2d84f9bbd52554074b
-&response_type=code
-&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback
-&scope=user-read-private%20user-read-email
-
-=end
-
-# https://accounts.spotify.com/authorize/?client_id=eca853c70bc24b2d84f9bbd52554074b&response_type=code&redirect_uri=http://50.71.192.157/spotify_callback&scope=user-read-private%20user-read-email
-# https://accounts.spotify.com/authorize/?client_id=eca853c70bc24b2d84f9bbd52554074b&response_type=code&redirect_uri=https://amplifyapp.ca/spotify_callback&scope=user-read-private%20user-read-email
-# https://accounts.spotify.com/authorize/?client_id=eca853c70bc24b2d84f9bbd52554074b&response_type=code&redirect_uri=https://amplifyapp.ca/spotify_callback&scope=user-read-private%20user-read-email
