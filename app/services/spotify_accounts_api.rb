bc7c2a5c6ba88722722bace59cb29c9168fc0f23
Robert Laurin <robert.laurin89@gmail.com>
2018-02-27 14:54:21 -0600

    Early splitting of the spotify account and v1 api services.

---
+++ app/services/spotify_accounts_api.rb
@@ -0,0 +1,38 @@
+require 'httparty'
+
+class SpotifyAccountsApi
+  include HTTParty
+
+  open_timeout 5
+  read_timeout 10
+  base_uri "https://accounts.spotify.com"
+
+  def fetch_tokens!(code)
+    response = send_request(:post, "api/token", {
+      query: { grant_type: "authorization_code", code: code, redirect_uri: Secrets[:spotify_redirect_uri] }
+    })
+    raise "something" if response.code != 200
+    response
+  end
+
+  def refresh_access_tokens!(refresh_token)
+    response = send_request(:post, "api/token" {
+      query: { grand_type: "refresh_token", refresh_token: refresh_token }
+    })
+    raise "something" if response.code != 200
+    response
+  end
+
+  private
+
+  def client_auth_value
+    ("Basic " + Base64.strict_encode64(Secrets[:spotify_client_id] + ":" + Secrets[:spotify_client_secret]))
+  end
+
+  def send_request(action, endpoint, options)
+    options[:headers] ||= {}
+    options[:headers]["Authorization"] = client_auth_value
+    response = self.class.public_send(:post, endpoint, options)
+    response.parsed_response
+  end
+end
