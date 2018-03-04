1ed0e5d4810596276183d0b6c8ead9567c8dbd26
Rebecca <becca.tiessen@gmail.com>
2018-03-04 17:17:03 -0600

    Added line back in

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -12,6 +12,7 @@ class SpotifyController < ApplicationController
 
   def set_api_by_host
     voter = Voter.find(session[:voter_id])
+    @spotify_api = voter.room.host.spotify_account.spotify_api
     unless @spotify_account.present?
       flash[:error] = "Search not available: the host has not linked their Spotify account"
       #render something
