d0a6e0ab9c627ebe18f1bc70926a4f23f5097c0d
Julia Stoyko <stoykojulia@gmail.com>
2018-03-05 15:12:41 -0600

    Next song endpoint

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -4,6 +4,8 @@ module Api
       include Data_Generator
       include Mapping
 
+      require 'json'
+
       def create
         return render json: { error: "Room limit reached!" }, status: :bad_request if @host.room.present?
 
@@ -46,6 +48,31 @@ module Api
           format.json { render json: { playlist: @playlist_data }, status: :ok }
           format.html {}
         end
+      end
+
+      def next_song
+        room = Room.find_by_id(params[:playlist_id])
+
+        max_score_song = room.songs[0]
+        max_score = max_score_song.votes.sum(:score)
+        locked_in_song = room.songs[0]
+        room.songs.each do |song|
+          if song.locked_in
+            locked_in_song = song
+          else
+            curr_score = song.votes.sum(:score)
+            if curr_score > max_score
+              max_score = curr_score
+              max_score_song = song
+            end
+          end
+        end
+
+        # return song locked_in=1
+        # and song with max votes and locked_in=0
+        # return song objects, not just ID's
+
+        [locked_in_song, max_score_song].to_json
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   constraints subdomain: "api" do
     scope module: 'api' do
       namespace 'v1' do
-        resources :rooms, only: [:create, :destroy, :index, :show]
+        resources :rooms, only: [:create, :destroy, :index, :show] do
+          get :next_song
+        end
         resources :hosts, only: [:create]
 
         get '/refresher/refresh_access_token', to: 'refresher#access_token' , as: :refresh_access
