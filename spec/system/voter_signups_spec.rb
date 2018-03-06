5eb7251918477407cd6b042deb88f70459841466
Robert Laurin <robert.laurin89@gmail.com>
2018-03-06 12:31:53 -0600

    Add some basic user interaction tests

--- spec/system/voter_signups_spec.rb
+++ spec/system/voter_signups_spec.rb
@@ -15,4 +15,35 @@ RSpec.describe "Voter Signups", :type => :system do
 
     expect(page).to have_current_path(playlist_path(room_a))
   end
+
+  it "notifies users if the room code is invalid" do
+    visit new_voter_signup_path
+
+    fill_in :room_code, with: "1142"
+    fill_in :nickname, with: "rob"
+    click_button "Join Room"
+
+    expect(page).to have_text("Ooops wrong code bruh")
+    expect(page).to have_current_path("/voter_signups")
+  end
+
+  it "roomcode is required" do
+    visit new_voter_signup_path
+
+    fill_in :nickname, with: "rob"
+    click_button "Join Room"
+
+    expect(page).to have_text("Ooops wrong code bruh")
+    expect(page).to have_current_path("/voter_signups")
+  end
+
+  it "nickname is required" do
+    visit new_voter_signup_path
+
+    fill_in :room_code, with: "1141"
+    click_button "Join Room"
+
+    expect(page).to have_text("Nickname can't be blank")
+    expect(page).to have_current_path("/voter_signups")
+  end
 end
