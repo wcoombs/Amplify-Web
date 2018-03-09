e562488f88e46abdcfcac9b98b107d89e27aff4c
Robert <robertlaurin@users.noreply.github.com>
2018-03-09 13:26:44 -0600

    Clamp vote values (#68)

--- spec/controllers/vote_controller_spec.rb
+++ spec/controllers/vote_controller_spec.rb
@@ -9,6 +9,20 @@ RSpec.describe VoteController, type: :controller do
   let(:song_b) { songs(:song_b) }
   let(:vote_a) { votes(:vote_a) }
 
+  it "doesn't allow people vote greater than 1" do
+    expect do
+      process(:update, params: { playlist_id: room_a.id, id: song_a2.id, vote: 1000 }, session: { voter_id: voter_a.id }, xhr: true)
+    end.to change { Vote.count }.by(1)
+    expect(song_a2.votes.sum(:score)).to eq(1)
+  end
+
+  it "doesn't allow people to vote less than -1" do
+    expect do
+      process(:update, params: { playlist_id: room_a.id, id: song_a2.id, vote: -1000 }, session: { voter_id: voter_a.id }, xhr: true)
+    end.to change { Vote.count }.by(1)
+    expect(song_a2.votes.sum(:score)).to eq(-1)
+  end
+
   it "doesn't allow people to vote on songs outside of their room" do
     expect do
       process(:update, params: { playlist_id: room_b.id, id: song_b.id }, session: { voter_id: voter_a.id }, xhr: true)
