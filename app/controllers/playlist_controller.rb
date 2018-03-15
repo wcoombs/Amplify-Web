2477558091f7a1c7141d76fc800a025489b39f23
Julia Stoyko <stoykojulia@gmail.com>
2018-03-15 16:14:51 -0500

    Add info to UI

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -14,9 +14,9 @@ class PlaylistController < ApplicationController
       return redirect_to root_path
     end
     current_song = @room.songs.where(song_status_id: song_statuses[:currently_playing]).first
-    current_song = current_song ? current_song.title : ""
+    current_song = current_song ? current_song.title : "No currently playing song"
     next_song = @room.songs.where(song_status_id: song_statuses[:up_next]).first
-    next_song = next_song ? next_song.title : ""
+    next_song = next_song ? next_song.title : "No songs in the queue"
     @playlist_data = {songs: playlist_data(@room.songs.where(song_status_id: song_statuses[:votable]), voter).sort_by{|s| -s[:total_score]},
                       current_song: current_song,
                       next_song: next_song}
